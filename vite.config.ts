cc4d853cd466fc1101745adfd75cdf638083b434
nitlimited <nitlimited0@gmail.com>
2026-02-04 23:30:52 +0000

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Moon_crochet_website_proposal/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
